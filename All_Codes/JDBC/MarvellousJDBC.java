3ec3fe8c0b4a370f912185e007e4fb76501abfce
mohitpatil1855 <mohitpatil0185@gmail.com>
2025-11-19 01:28:44 +0530

    Add files via upload

---
+++ All_Codes/JDBC/MarvellousJDBC.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+class MarvellousJDBC {
+    public static void main(String[] args) {
+
+        try {
+            // Step 1 : Load the database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Step 2 : Connect the driver with database
+            Connection cobj = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/PPA53",
+                "root",
+                ""
+            );
+
+            // Step 3 : Create statement class object
+            Statement sobj = cobj.createStatement();
+
+            // Step 4 : Execute the Query
+            ResultSet robj = sobj.executeQuery("SELECT * FROM student");
+
+            // Step 5 : Display the result
+            while (robj.next()) {
+                System.out.println("Roll no : " + robj.getInt("Rno"));
+                System.out.println("Name : " + robj.getString("Name"));
+                System.out.println("City : " + robj.getString("City"));
+                System.out.println("Marks : " + robj.getInt("Marks"));
+                System.out.println("----------------------------");
+            }
+
+            robj.close();
+            sobj.close();
+            cobj.close();
+
+        } catch (Exception eobj) {
+            System.out.println("Exception occurred : " + eobj);
+        }
+    }
+}
